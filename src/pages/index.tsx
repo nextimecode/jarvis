833daf841dac2f597299322cf7cef3da9f6919c2
Pedro Duarte <pedrohlduarte@gmail.com>
2021-10-10 22:24:14 -0300

    feat: add data da pagina index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,7 @@
 
 import React from 'react'
-import { NextFeatureProps } from '../components/molecules/nextFeature'
 import NextTemplateHome from '../components/templates/nextTemplateHome'
-
-const nextFeatureItems = [{
-  title: 'Tecnologia',
-  text: 'Usamos o que há de melhor no mercado, entregando soluções modernas. Adicionamos tecnologias de inteligência Artificial e estratégias analíticas para o seu negócio.',
-  icon: ''
-}] as unknown as Array<NextFeatureProps>
+import { nextFeatureItems } from './data'
 
 const NextHome = () => {
   return (
