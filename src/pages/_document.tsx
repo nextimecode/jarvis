57c5216d305907c446312f32e83b846333ec907e
Pedro Duarte <pedrohlduarte@gmail.com>
2021-11-07 16:20:18 -0300

    feat: add tag do google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,17 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <meta name="description" content="Site da empresa NeXTIME, temos como visão: otimizar o tempo através do desenvolvimento de novas tecnologias, produtos inovadores, soluções criativas e educação. Possibilitando aos indivíduos gastarem suas energias realizando os seus sonhos." />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-CT81W4DWTK" />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-CT81W4DWTK');
+            `
+            }}
+          />
         </Head>
         <body>
           <Main />
