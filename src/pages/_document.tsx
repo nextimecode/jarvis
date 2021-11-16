78c84e434c0898c44f555c768405a49e36217d82
Pedro Duarte <pedrohlduarte@gmail.com>
2021-11-16 11:28:33 -0300

    fix: google tagmanager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,14 +7,14 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <meta name="description" content="Site da empresa NeXTIME, temos como visão: otimizar o tempo através do desenvolvimento de novas tecnologias, produtos inovadores, soluções criativas e educação. Possibilitando aos indivíduos gastarem suas energias realizando os seus sonhos." />
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-YBBVTWFKF4" />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-HN7P9PMXX2" />
           <script
             dangerouslySetInnerHTML={{
               __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-YBBVTWFKF4');
+              gtag('config', 'G-HN7P9PMXX2');
             `
             }}
           />
