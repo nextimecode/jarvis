6ed54ddcd6e9dc80f91037bee630aa6999b27ac0
Pedro Duarte <pedrohlduarte@gmail.com>
2022-11-19 19:08:21 -0300

    fix: importacao do nextlayou

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,11 +1,11 @@
 import NextArticleList from '../../components/organisms/nextArticleList'
-import NextLayout from '../../components/templates/NextLayout'
+import NextLayout from '../../components/templates/nextLayout'
 import { useGetPostsQuery } from '../../graphql/generated'
 
 export default function Blog() {
   const { data } = useGetPostsQuery()
   return (
-    <NextLayout isLogged={false}>
+    <NextLayout>
       <NextArticleList posts={data?.posts} />
     </NextLayout>
   )
