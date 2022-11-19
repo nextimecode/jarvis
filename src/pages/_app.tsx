d7b09822d037bd2f91e7761beb036d4b7da313b7
Pedro Duarte <pedrohlduarte@gmail.com>
2022-11-19 18:59:47 -0300

    feat: add apolo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,13 +9,16 @@ import '@fontsource/open-sans/600.css'
 import '@fontsource/open-sans/700.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
-
+import { ApolloProvider } from '@apollo/client'
+import { client } from '../lib/apollo'
 import theme from '../../theme/'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
     </ChakraProvider>
   )
 }
