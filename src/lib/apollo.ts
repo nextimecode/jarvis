fd983985a7353a237a2c853e7d7e7d41380615dc
Pedro Duarte <pedrohlduarte@gmail.com>
2023-03-30 22:59:44 -0300

    fix: update appolo

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,27 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
+import { RetryLink } from '@apollo/client/link/retry'
 
-export const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_APOLLO_URI,
-  cache: new InMemoryCache(),
+const cache = new InMemoryCache()
+
+const retryLink = new RetryLink({
+  delay: {
+    initial: 1000, // Tempo inicial de atraso em milissegundos
+    max: 10000, // Tempo máximo de atraso em milissegundos
+    jitter: true, // Adicionar ou subtrair um fator aleatório ao atraso
+  },
+  attempts: {
+    max: 5, // Número máximo de tentativas
+    retryIf: (error, operation) => {
+      return error.status === 429
+    },
+  },
 })
+
+const httpLink = new HttpLink({ uri: process.env.NEXT_PUBLIC_APOLLO_URI })
+
+const client = new ApolloClient({
+  link: retryLink.concat(httpLink),
+  cache,
+})
+
+export { client }
