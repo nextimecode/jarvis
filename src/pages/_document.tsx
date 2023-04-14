4792d1d899905eb6067665232ef9eb4c83b56539
Pedro Duarte <pedrohlduarte@gmail.com>
2023-04-14 12:37:01 -0300

    fix: replace to final file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,9 +18,9 @@ export default function Document() {
           gtag('config', 'G-HN7P9PMXX2');
         `}
         </Script>
-        {/* <script
-            dangerouslySetInnerHTML={{
-              __html: `
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
               (function(h,o,t,j,a,r){
                 h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
                 h._hjSettings={hjid:2699382,hjsv:6};
@@ -29,13 +29,27 @@ export default function Document() {
                 r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
                 a.appendChild(r);
               })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-            `
-            }}
-          /> */}
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
         <NextScript />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:2699382,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+          }}
+        />
       </body>
     </Html>
   )
