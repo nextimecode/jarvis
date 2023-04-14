1c3490b9fb760faf569aa5d0a8ea43a6624569e2
Pedro Duarte <pedrohlduarte@gmail.com>
2023-04-14 13:19:39 -0300

    fix: change ordem in hotjar and add strategy lazyonload (#65)
    * fix: change ordem in hotjar and add strategy lazyonload
    
    * fix: return for to the head
    
    * fix: return for to the head
    
    * fix: hotjar after interactive

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,22 @@ export default function Document() {
     <Html lang="pt-BR">
       <Head>
         <Script
+          id="hotjar"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:2699382,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+          }}
+        />
+        <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-HN7P9PMXX2"
           strategy="afterInteractive"
         />
@@ -18,38 +34,10 @@ export default function Document() {
           gtag('config', 'G-HN7P9PMXX2');
         `}
         </Script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:2699382,hjsv:6};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-            `,
-          }}
-        />
       </Head>
       <body>
         <Main />
         <NextScript />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:2699382,hjsv:6};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-            `,
-          }}
-        />
       </body>
     </Html>
   )
